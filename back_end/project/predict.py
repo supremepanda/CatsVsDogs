ce6bd5e71b2c70fa778e7fc1e4f28ce148966b19
supremepanda <furkanbaldir13@gmail.com>
2021-01-14 00:23:22 +0300

    training model was created, predict function was created, the codes will be beautified.
    training model was created, predict script was created, the code will be beautified.
    
    Codes beautifying done
    
    All pr tasks done.
    
    Function comments have been edited. For cats and dogs, there are two constant integer variables used, print() lines removed, returns edited, loading and saving file utility functions created.

---
+++ back_end/project/predict.py
@@ -0,0 +1,62 @@
+from sklearn.svm import SVC
+import numpy
+from PIL import Image
+import file_management
+
+CAT = 0
+DOG = 1
+
+# It converts image to numpy array
+def convert_image_numbers(img_path):
+    
+    pet_img = Image.open(img_path)
+    pet_img = pet_img.resize((50, 50))
+    image = numpy.asarray(pet_img).flatten()
+    image = image.reshape(1, 7500)
+    
+    return image
+    
+# It predicts label of given image, cat or dog
+def predict(img_path):
+
+    # Loading the model
+    model = file_management.load_file('model.sav')
+
+    # Getting image from convert_image_numbers() function
+    image = convert_image_numbers(img_path)
+
+    # Predicting label (cat or dog?)
+    predicted_label = model.predict(image)
+
+    if(predicted_label == 0):
+        return CAT
+    return DOG
+        
+
+# It predicts with given test data and calculates accuracy
+def predict_with_test_data():
+
+    # Loading the model
+    model = file_management.load_file('model.sav')
+    
+    # Loading the test data
+    data = file_management.load_file('test.pickle')
+
+    # Initializing empty arrays for features and labels
+    features = []
+    labels = []
+
+    # For feature and label in data ([image, label]), appending to arrays
+    for feature, label in data:
+        features.append(feature)
+        labels.append(label)
+
+    # Predicting with test data
+    model.predict(features)
+
+    # Calculating accuracy of prediction
+    accuracy = model.score(features, labels)
+
+    return accuracy
+
+#predict_with_test_data()
---
+++ back_end/project/training_model_creating.py
@@ -0,0 +1,119 @@
+import os
+import numpy
+from sklearn.svm import SVC
+from PIL import Image
+import file_management
+
+CATS = 0
+DOGS = 1
+
+# It stores test data to get accuracy of our Support Vector Classifier
+def pick_test_data():
+
+    # Path of test data
+    directory = r'D:\CatsVsDogs\back_end\project\testdata'
+
+    # Categories
+    categories = [CATS, DOGS]
+
+    # Empty data array
+    data = []
+
+    # For every category
+    for category in categories:
+
+        # Getting sub-directory path
+        path = os.path.join(directory, category)
+
+        # Setting label of image
+        label = categories.index(category)
+    
+        # For each image...
+        for img in os.listdir(path):
+            # Getting image path
+            img_path = os.path.join(path, img)
+
+            # Loading image from img_path
+            pet_img = Image.open(img_path)
+
+            # Resizing pet image
+            pet_img = pet_img.resize((50, 50))
+
+            # Converting image to 1D array
+            image = numpy.asarray(pet_img).flatten()
+
+            # Appending data with category (label)
+            data.append([image, label])
+
+    # Saving pre-proceed test data as 'test.pickle'
+    file_management.save_file('test.pickle', data)      
+
+# It stores data as pickle filetype.
+def pick_all_data():
+    # The path of the images directory
+    directory = r'D:\CatsVsDogs\back_end\project\dataset'
+
+    # The array of categories that these are 'cats' and 'dogs'
+    categories = [CATS, DOGS]
+
+    # The initialization of empty data array
+    data = []
+    
+    # For every category...
+    for category in categories:
+
+        # Getting sub-directory path
+        path = os.path.join(directory, category)
+
+        # Setting label of image
+        label = categories.index(category)
+    
+        # For each image...
+        for img in os.listdir(path):
+            # Getting image path
+            img_path = os.path.join(path, img)
+
+            # Loading image from img_path
+            pet_img = Image.open(img_path)
+
+            # Resizing pet image
+            pet_img = pet_img.resize((50, 50))
+
+            # Converting image to 1D array
+            image = numpy.asarray(pet_img).flatten()
+
+            # Appending data with category (label)
+            data.append([image, label])
+            
+            
+    # Save pre-proceed train data as 'data.pickle' 
+    file_management.save_file('data.pickle', data)   
+
+# It opens our train data and creates a new model
+def train_data():
+    
+    # Loading the training data
+    data = file_management.load_file('data.pickle')
+
+    # Initializing empty arrays as features and labels
+    features = []
+    labels = []
+
+    # For feature and label in data ([image, label]), appending to arrays
+    for feature, label in data:
+        features.append(feature)
+        labels.append(label)
+    
+    # Creating Support Vector Classifier model
+    model = SVC(C=1, kernel='poly', gamma='auto')
+
+    # Fitting model
+    model.fit(features, labels)
+
+    # Saving fitted model as 'model.sav'
+    file_management.save_file('model.sav', model)
+
+#pick_all_data()
+#train_data()
+#pick_test_data()
+
