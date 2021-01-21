55237824efe4dda56ae4748c529a58910f1b3727
mehmetpekcan <pekcanmehmett@gmail.com>
2021-01-22 00:22:26 +0300

    deployment branch init

---
+++ api/file_management.py
@@ -0,0 +1,49 @@
+import pickle
+import os
+import zipfile
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# It checks 'model.sav' file is exist
+def check_sav_file():
+    for i in os.listdir():
+        if(i == 'model.sav'):
+            return True
+    return False
+
+# It unzip the 'model.zip'
+def unzip_model():
+    with zipfile.ZipFile(f'{ROOT_DIR}/model.zip', 'r') as zip_ref:
+        zip_ref.extractall()
+
+# It loads a pickle file 
+def load_file(filename):
+    
+    if(filename == 'model.sav'):
+        if(check_sav_file() == False):
+            unzip_model()
+
+
+    # Opening the file
+    pick = open(f'{filename}', 'rb')
+
+    # Storing the file data with temporary local variable
+    model = pickle.load(pick)
+
+    # Closing the file
+    pick.close()
+
+    # Returning the data
+    return model
+
+# It saves a pickle file
+def save_file(filename, data):
+
+    # Opening the file
+    pick = open(f'{ROOT_DIR}/{filename}', 'wb')
+
+    # Saving the file
+    pickle.dump(data, pick)
+
+    # Closing the file
+    pick.close()
--- api/main.py
+++ api/main.py
@@ -1,19 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-
-# It handles importing problem to import our local directory
-import sys
-sys.path.insert(1, 'backend')
-
 import predict
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build', static_url_path='')
 CORS(app)
 
 # Getting the predict result from API
 @app.route("/api/predict-image", methods=['POST'])
 def predictImage():
 	file = request.files['image']
-	response = { "type": predict.predict(file) }
+	response = jsonify({ "type": predict.predict(file) })
 
 	return response
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", threaded=True, port=5000)
