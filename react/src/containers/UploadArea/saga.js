f199c76c6ce7791a0a806e7cf5db9f62b8f79e23
mehmetpekcann <mehmet.pekcan@getir.com>
2021-01-22 19:53:41 +0300

    heroku config

--- react/src/containers/UploadArea/saga.js
+++ react/src/containers/UploadArea/saga.js
@@ -1,25 +1,26 @@
-import { takeLatest, call, put } from "redux-saga/effects"
-import uploadAreaActions, { uploadAreaTypes } from "./actions"
+import { takeLatest, call, put } from "redux-saga/effects";
+import uploadAreaActions, { uploadAreaTypes } from "./actions";
 
-const BASE_URL = "http://127.0.0.1:5000"
+const BASE_URL = "https://client-cats-vs-dogs.herokuapp.com";
 
 async function callPredictImage(image) {
 	const { type } = await fetch(`${BASE_URL}/api/predict-image`, {
-		method: 'POST',
-		body: image
-	  }).then(res => res.json())
+		method: "POST",
+		body: image,
+		mode: "cors",
+	}).then((res) => res.json());
 
-	return type
+	return type;
 }
 
 export function* uploadImageSaga({ image }) {
-	const form = new FormData()
-	form.append("image", image)
+	const form = new FormData();
+	form.append("image", image);
 
-	const imageType = yield call(callPredictImage, form)
-	yield put(uploadAreaActions.uploadImageSuccess(imageType))
+	const imageType = yield call(callPredictImage, form);
+	yield put(uploadAreaActions.uploadImageSuccess(imageType));
 }
 
 export default function* uploadAreaSaga() {
-	yield takeLatest(uploadAreaTypes.UPLOAD_IMAGE, uploadImageSaga)
+	yield takeLatest(uploadAreaTypes.UPLOAD_IMAGE, uploadImageSaga);
 }
